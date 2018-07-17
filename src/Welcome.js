a01e799a8e8500c535b8b1aa085b869dc44ba68f
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2018-07-17 15:59:39 +0200

    Add a header and router, to navigation on page.

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Welcome extends Component {
+  render() {
+    return(
+      <h1> Welcome to my home Page</h1>
+    )
+  }
+}
+
+export default Welcome;
