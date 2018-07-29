95353460878b1172aea7c2509e22757b7457dee4
Vivian Machindano <vivianmachindano@Vivians-MacBook-Pro.local>
2018-07-29 15:47:44 +0200

    frontpage layout version 1 is done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,82 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Layout, Drawer, Navigation, Content, Header } from 'react-mdl';
 import Main from './main';
 import { Link } from 'react-router-dom';
+import CssModules from 'react-css-modules';
+import styles from './Resume.css';
+import {Layout, Content, Header, Navigation, Drawer, Footer, FooterLinkList, FooterSection, Grid, Cell } from 'react-mdl'
+import Welcome from './Welcome';
+
+
 
 class App extends Component {
   render() {
-    return (
+    return (    <div>
 
-      <div style={{height: '300px', position: 'relative'}}>
-    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
-        <Header transparent title="Vivian Machindano" style={{color: 'white'}}>
+
+<div style={{height: '300px', position: 'relative'}}>
+<div>
+          
+    <Layout fixedHeader>
+
+        <Header transparent title={<span><span >  </span><strong>Vivian Seerup</strong></span>}>
+
             <Navigation>
-                 <Link to="/Welcome">Welcome</Link>
+               
+                <Link to= "/">Welcome</Link>
                 <Link to="/Resume">Resume</Link>
-                <Link to="/Blog">Blog</Link>
-                <Link to="/Portfolio">Portfolio</Link>
+                
             </Navigation>
+            
+
         </Header>
         <Drawer title="Title">
             <Navigation>
-                 <Link to="/Welcome">Welcome</Link>
+               <Link to="/">Welcome</Link>
                 <Link to="/Resume">Resume</Link>
-                <Link to="/Blog">Blog</Link>
-                <Link to="/Portfolio">Portfolio</Link>
+                
             </Navigation>
         </Drawer>
-        <Content />
+        <Content >
+        <div className="page-content" />
+         
+           </Content> 
     </Layout>
+     <Main/>
+
+     <div>
+
+<Footer size="mini">
+    <FooterSection type="left" logo="">
+        <FooterLinkList><div class="si">
+
+<div >
+
+    
+    <Grid className="demo-grid-ruler"><p class="si">
+        <Cell col={4}> <a href="https://www.linkedin.com/in/vivian-luisa-seerup-24a792bb/"><i class="fa fa-linkedin si"></i></a></Cell>
+        <Cell col={4}> <a href="https://www.facebook.com/profile.php?id=100007712745224"><i class="fa fa-facebook"></i></a></Cell>
+        <Cell col={4}> <i class="fa fa-envelope">vivianseerup@gmail.com</i></Cell>
+        <Cell col={4}> <i class="fa fa-phone-square">+4553638035</i></Cell></p>
+    </Grid>
+    
 </div>
 
+
+           
+            </div>
+        </FooterLinkList>
+    </FooterSection>
+</Footer>
+
+</div>
+</div> </div>
+
+
+
+
+ </div>
 
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,111 @@
 import React, { Component } from 'react';
+import './Resume.css';
+import { Parallax, Background } from 'react-parallax';
+import {Grid, Cell, Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton} from 'react-mdl'
+import Image from './assets/sound.png';
+import Image1 from './assets/africa.png';
+import Image2 from './assets/portfolio.png';
+
+const MyComponent = () => (
+  <div>
+
+  <div>
+    <Parallax
+      blur={0.5}
+      bgImage={require('./assets/header.jpeg')}
+      bgImageAlt="the cat"
+      strength={200}
+      bgheight={'auto'}
+      opacity='0%'
+    >
 
 
-class Welcome extends Component {
-  render() {
-    return(
-      <h1> Welcome to my home Page</h1>
-    )
-  }
-}
+      <div style={{ height: '400px' }} >
 
-export default Welcome;
+      <h1 class="aviso">LOOKING FOR A JUNIOR WEB DEVELOPER</h1> </div>
+    </Parallax>
+
+</div>
+
+<div class="carde">
+
+ <p ><strong> JUNIOR WEB DEVELOPER | IT ENGINEER
+
+</strong> </p>
+
+<p>Hi, I’m Vivian. Nice to meet you.
+
+I am 25 years old Junior web developer with a 5 years bachelors degree in IT
+engineering seeking new challenges.
+I am positive, organized and team oriented in the
+way I work. My main focus has been React,
+Wordpress and Ruby on Rails. 
+During the studies, I worked a bit with php, java(Eclipse), C++, Matlab,
+ what would make easier to quickly understand and learn the tools you a using at your company.</p>
+<p>  As a recente Graduate, i'm looking forward to show all my skills in this new carreer.</p><br/> <br/>
+  
+</div>
+
+<div>
+     <Parallax
+      blur={0.5}
+      bgImage={require('./assets/barco.jpg')}
+      bgImageAlt="the cat"
+      strength={200}
+      bgheight={'auto'}
+      offsetYMax={20}
+    >
+
+      <div style={{ height: '500px' }} />
+    </Parallax>
+
+
+    <div class="trabalho">
+
+<div></div>
+<div><p><stromg class="titulos">PROJECTS </stromg></p></div>
+<div>
+
+ <Grid className="demo-grid-1">
+        <Cell col={4}><Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+    <CardTitle expand style={{color: '#fff', backgroundImage: `url(${Image1})`}}>Update</CardTitle>
+    <CardText>
+        A Blog to show all the restaurant,Bars and events happening in Copenhagen. I'm working with Wordpress.
+    </CardText>
+    <CardActions border>
+        <a href="http://africaincopenhagen.com/"><Button colored>Check out</Button></a>
+    </CardActions>
+</Card></Cell>
+        <Cell col={4}><Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+    <CardTitle expand style={{color: '#fff',  backgroundImage: `url(${Image})`}}></CardTitle>
+    <CardText>
+        Participation on a music website working with react.
+    </CardText>
+    <CardActions border>
+       <a href="https://www.soundtracktor.com/en"><Button colored>Check out</Button></a>
+    </CardActions>
+</Card></Cell>
+        <Cell col={4}><Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
+    <CardTitle expand style={{color: '#fff', backgroundImage: `url(${Image2})`}}></CardTitle>
+    <CardText>
+       My personal Portfolio and blog, its a website that i'm working on it using react and node.js
+    </CardText>
+    <CardActions border>
+        <a href="http://www.viluma.dk/"><Button colored>Check out</Button></a>
+    </CardActions>
+</Card></Cell>
+    </Grid>
+  </div>
+
+
+<div><p class="">I'm available for work, get in touch
+ </p></div>
+
+
+</div>
+    
+</div>
+
+  </div>
+);
+export default MyComponent;
